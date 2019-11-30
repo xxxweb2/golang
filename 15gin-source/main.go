69f829e09e4ae0904b2174cbcbe9f17187bcb44d
xuxinxin <1070529431@qq.com>
2019-11-30 17:21:08 +0800

    gin source study

---
+++ 15gin-source/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	c := gin.Default()
+
+	c.GET("/test", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+	c.GET("/t/:name/c", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "hello name",
+		})
+	})
+	c.Run()
+}
