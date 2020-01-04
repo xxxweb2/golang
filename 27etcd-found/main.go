4c4d69886dc18ba2831c30c2d4d774c796dec62c
xuxinxin <1070529431@qq.com>
2020-01-04 14:54:23 +0800

    etcd服务发现

---
+++ 27etcd-found/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"context"
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/mvcc/mvccpb"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
+
+type ClientDis struct {
+	client     *clientv3.Client
+	serverList map[string]string
+	lock       sync.Mutex
+}
+
+func NewClientDis(addr []string) (*ClientDis, error) {
+	conf := clientv3.Config{
+		Endpoints:   addr,
+		DialTimeout: 5 * time.Second,
+	}
+	client, err := clientv3.New(conf)
+
+	if err == nil {
+		return &ClientDis{
+			client:     client,
+			serverList: make(map[string]string),
+		}, nil
+	} else {
+		return nil, err
+	}
+}
+
+func (this *ClientDis) GetService(prefix string) ([]string, error) {
+	resp, err := this.client.Get(context.TODO(), prefix, clientv3.WithPrefix())
+	if err != nil {
+		return nil, err
+	}
+
+	addrs := this.extractAddrs(resp)
+	go this.watcher(prefix)
+
+	return addrs, nil
+}
+
+func (this *ClientDis) extractAddrs(resp *clientv3.GetResponse) []string {
+	addrs := make([]string, 0)
+	if resp == nil || resp.Kvs == nil {
+		return addrs
+	}
+	for i := range resp.Kvs {
+		if v := resp.Kvs[i].Value; v != nil {
+			this.SetServiceList(string(resp.Kvs[i].Key), string(resp.Kvs[i].Value))
+			addrs = append(addrs, string(v))
+		}
+	}
+
+	return addrs
+}
+
+func (this *ClientDis) SetServiceList(key, val string) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+	this.serverList[key] = val
+
+	log.Println("set data key :", key, "val:", val)
+}
+
+func (this *ClientDis) watcher(prefix string) {
+	rch := this.client.Watch(context.Background(), prefix, clientv3.WithPrefix())
+	for wresp := range rch {
+		for _, ev := range wresp.Events {
+			switch ev.Type {
+			case mvccpb.PUT:
+				this.SetServiceList(string(ev.Kv.Key), string(ev.Kv.Value))
+			case mvccpb.DELETE:
+				this.DelServiceList(string(ev.Kv.Key))
+			}
+		}
+	}
+}
+
+func (this *ClientDis) DelServiceList(key string) {
+	this.lock.Lock()
+	defer this.lock.Unlock()
+	delete(this.serverList, key)
+	log.Println("del data key:", key)
+}
+
+func main() {
+	cli, _ := NewClientDis([]string{"198.13.34.80:2379"})
+	res, _ := cli.GetService("/node")
+	fmt.Println(res)
+	select {}
+}
