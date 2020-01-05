69ad2524988635782f7ae531db7549e32fb66e0f
xuxinxin <1070529431@qq.com>
2020-01-05 23:03:23 +0800

    kafka 生产者

---
+++ 30kafka-pro/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+)
+
+var Address = []string{"198.13.34.80:9092"}
+
+func main() {
+}
+
+func producerTest() {
+	fmt.Printf("producer_test\n")
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.Return.Successes = true
+	config.Producer.Return.Errors = true
+
+	producer, err := sarama.NewAsyncProducer([]string{"198.13.34.80:9092"}, config)
+	if err != nil {
+		fmt.Printf("producer_test create producer error :%s\n", err.Error())
+		return
+	}
+	defer producer.AsyncClose()
+
+	msg := &sarama.ProducerMessage{
+		Topic: "kafka_go_test",
+		Key:   sarama.StringEncoder("go_test"),
+	}
+
+	value := "this is message"
+
+	for {
+		fmt.Scan(&value)
+		msg.Value = sarama.ByteEncoder(value)
+		fmt.Printf("input [%s]\n", value)
+
+		producer.Input() <- msg
+
+		select {
+		case suc := <-producer.Successes():
+			fmt.Printf("offset: %d,  timestamp: %s", suc.Offset, suc.Timestamp.String())
+		case fail := <-producer.Errors():
+			fmt.Printf("err: %s\n", fail.Err.Error())
+		}
+	}
+}
